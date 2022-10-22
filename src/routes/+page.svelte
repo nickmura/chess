81c32e7e6fa695549eb8f64add492151da08bcf8
nickmura <92566574+nickmura@users.noreply.github.com>
2022-10-21 21:18:24 -0600

    Revert "Added draft for chess smart contract game"
    This reverts commit 12724d2d93480c3a09552c4e88b8ecfb2c4954fe.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,21 @@
 <script>
 		// @ts-nocheck
-		import { writable } from 'svelte/store';
-		import { browser } from '$app/environment';
+		import { writable } from 'svelte/store'
+		import { browser } from '$app/environment'
 		import '$lib/cgstyles/chessground.css';
-		import { io } from 'socket.io-client';
+		import { io } from 'socket.io-client'
 		import { goto } from '$app/navigation';
 
-		import Auth from '../lib/Auth.svelte';
+
 		const socket = io("http://localhost:3000")
 
 		let counter=0
 		let rooms = []
 		let currentRoom
-		let currentGame
+
 
 		if (browser) {
 			currentRoom = sessionStorage.getItem('roomID')
-
 		}	
 		async function getRooms() {
 			const res = await fetch('http://localhost:5001/rooms')
@@ -25,26 +24,26 @@
 		}
 		setInterval(getRooms, 1000)
 
-		let isMade
+
 		function createChessRoom() {
-			isMade = true
 			counter++;
 			let uuid = Math.floor(Math.random()*10000)
 			sessionStorage.setItem('roomID', uuid)
-			let room = {gameID: uuid, game: 'chess', players: [], isCheckmate: 'false', isDraw: 'false', fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', stake: '0'}
+			let room = {gameID: uuid, game: 'chess', players: [], fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', stake: '0'}
 			room.players.push(`player${counter}`)
 			socket.emit('createRoom', uuid, room)
 			goto('./play')
-
 		}
 
 
 		function joinGame(gameId) {
 			counter++;
 			let player2 = `player${counter}`
-			sessionStorage.setItem('roomID', gameId)
-			rooms.find(room => room.gameID === gameId).players.push(player2)
 
+			sessionStorage.setItem('roomID', gameId)
+
+			rooms.find(room => room.gameID === gameId).players.push(player2)
+			goto('./play')
 			// need to do socket.emit and send back info to server
 			socket.emit("joinRoom", player2, gameId)
 			goto('./play')
@@ -54,8 +53,7 @@
 			goto('./play')
 		}
 	</script>
-
-	<Auth></Auth>
+	
 	{#if currentRoom}
 	<div style='font-size: 3rem;'>It seems you have a room already. Click<button class='btn btnlink' 
 		style='font-size: 3rem;'><u style='color: blue;' on:click={gotoGameRoom}>here</u></button> to go your game!</div>
@@ -90,7 +88,7 @@
 				<button on:click={(e)=>joinGame(room.gameID)} class='btn btn-primary'> 
 					Join Game
 				</button>
-				{:else if currentRoom || isMade}
+				{:else if currentRoom}
 					<button on:click={(e)=>joinGame(room.gameID)} class='btn btn-primary disabled' disabled> 
 						Join Game
 					</button>
